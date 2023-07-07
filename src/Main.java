cf1e54b89b8dedb7201a151a20ee913b0c45402e
StellaAnyanwu <stellaanyanwu94@gmail.com>
2023-07-07 11:20:17 +0100

    git commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello and welcome to my project");
+        System.out.println("My name is stella, i am an aspiring software tester");
+        System.out.println("Its been a rewarding journey for me and all thanks to my team mates and my mentors");
+    }
+
+}
+
