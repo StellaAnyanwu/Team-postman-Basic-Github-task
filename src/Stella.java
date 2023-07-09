76968de762c03cadac07746e05e1b5b928b210be
StellaAnyanwu <stellaanyanwu94@gmail.com>
2023-07-09 16:19:09 +0100

    git commit

---
+++ src/Stella.java
@@ -0,0 +1,7 @@
+public class Stella {
+    public static void main(String[] args) {
+        System.out.println("Hello, I am stella. I am happy to be here");
+        System.out.println("It is my first Java experience");
+    }
+}
+
